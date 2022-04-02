87aa27ac64a866abed64cde2b9bf462dfea105ce
Petr B Nylander <32863524+Cerlog@users.noreply.github.com>
2022-04-02 12:37:53 +0200

    Vertex - made fields into private, comparable implemented, generated getters and setters

--- src/com/company/Vertex.java
+++ src/com/company/Vertex.java
@@ -1,4 +1,56 @@
 package com.company;
 
-public class Vertex {
+import java.util.ArrayList;
+
+public class Vertex implements Comparable<Vertex> {
+    private String cityName;              // city name
+    private Integer distance;             // distance
+    private Vertex predecessor;           // predecessor
+    private ArrayList<Edge> outerEdge;    //
+
+    public Vertex(String cityName) {
+        this.cityName = cityName;
+        this.outerEdge = new ArrayList<>();
+    }
+
+    @Override                        // compareTo method for comparing the verticies
+    public int compareTo(Vertex other) {
+        return this.distance.compareTo(other.distance)
+    }
+
+
+    // generated getters and setters
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Integer distance) {
+        this.distance = distance;
+    }
+
+    public Vertex getPredecessor() {
+        return predecessor;
+    }
+
+    public void setPredecessor(Vertex predecessor) {
+        this.predecessor = predecessor;
+    }
+
+    public ArrayList<Edge> getOuterEdge() {
+        return outerEdge;
+    }
+
+    public void setOuterEdge(ArrayList<Edge> outerEdge) {
+        this.outerEdge = outerEdge;
+    }
+
 }
