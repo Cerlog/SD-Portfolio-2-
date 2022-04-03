e646b4a02057af7a7ee801087d91fb9b132447f1
Petr B Nylander <32863524+Cerlog@users.noreply.github.com>
2022-04-03 10:17:56 +0200

    AdjacencyList : added comments

--- src/com/company/AdjacencyList.java
+++ src/com/company/AdjacencyList.java
@@ -4,26 +4,26 @@ import java.util.ArrayList;
 
 public class AdjacencyList {
 
-    private ArrayList<Vertex> listOfVerticies;
+    private ArrayList<Vertex> listOfVerticies;              // stores the list of verticies
 
     public AdjacencyList() {
         this.listOfVerticies = new ArrayList<>();
-    }
+    }  // stores the list of verticies
 
     public void addVertex(Vertex vertex) {
         listOfVerticies.add(vertex);
-    }
+    }  // method for adding vertices to the list
 
-    public void addUndirectedWeightedEdge(Vertex fromVertex, Vertex toVertex, Integer weight) {
+    public void addUndirectedWeightedEdge(Vertex fromVertex, Vertex toVertex, Integer weight) {   // method for adding undirected edges to the list
         if (!listOfVerticies.contains(fromVertex) && listOfVerticies.contains(toVertex)) {
             System.out.println("Vertex is missing");
             return;
         }
-        Edge newEdgeFromTO = new Edge(fromVertex,toVertex,weight);
-        Edge newEdgeToFrom = new Edge(toVertex,fromVertex,weight);
+        Edge newEdgeFromTO = new Edge(fromVertex,toVertex,weight);                               // creates edge from vertex1 to other vertex2
+        Edge newEdgeToFrom = new Edge(toVertex,fromVertex,weight);                               // creates edge from vertex2 to vertex 1
     }
 
-    public void printAdjacencyGraph () {
+    public void printAdjacencyGraph () {                                                       // method for printing the graphs
         for (int i = 0; i < listOfVerticies.size(); i++) {
             System.out.println("Vertex " + listOfVerticies.get(i).getCityName() + " is connected to: ");
             Vertex currentVertex = getListOfVerticies().get(i);
@@ -32,13 +32,7 @@ public class AdjacencyList {
             }
         }
     }
-
-
     public ArrayList<Vertex> getListOfVerticies() {
         return listOfVerticies;
-    }
-
-    public void setListOfVerticies(ArrayList<Vertex> listOfVerticies) {
-        this.listOfVerticies = listOfVerticies;
-    }
+    }                // getter method for te
 }
